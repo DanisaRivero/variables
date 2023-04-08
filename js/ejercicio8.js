9dbd5f7cbf582acfba88dd1242b51d2dc4709108
Danisa <danisarivero@gmail.com>
2023-04-08 11:58:17 -0300

    tp1 - 8

---
+++ js/ejercicio8.js
@@ -0,0 +1,7 @@
+//* Escribe un programa que pida un número y diga si es divisible por 2
+let number = parseInt(prompt("Escribe un número"));
+if (number % 2 == 0) {
+  document.write("El numero ingresado es divisible por 2");
+} else {
+  document.write("El numero ingresado no es divisible por 2");
+}
